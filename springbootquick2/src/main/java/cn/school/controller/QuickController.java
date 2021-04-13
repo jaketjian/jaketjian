9797c43a1d6dd458c41af655c2050ba0a0916bba
jaketjian <jaketjian@xr.cn>
2021-04-13 18:21:38 +0800

    修改springboot

--- springbootquick2/src/main/java/cn/school/controller/QuickController.java
+++ springbootquick2/src/main/java/cn/school/controller/QuickController.java
@@ -2,11 +2,13 @@ package cn.school.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class QuickController {
     @RequestMapping("/quick")
     public String quick(){
+        ModelAndView mv =new ModelAndView();
         return "hello Nicetommetcontroller";
     }
 }
