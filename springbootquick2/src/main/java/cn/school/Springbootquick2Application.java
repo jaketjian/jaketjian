7f510f23dca89cc7df97e2eab2aa42fae6f7caec
jaketjian <jaketjian@xr.cn>
2021-04-13 18:53:24 +0800

    修改controller

--- springbootquick2/src/main/java/cn/school/Springbootquick2Application.java
+++ springbootquick2/src/main/java/cn/school/Springbootquick2Application.java
@@ -9,5 +9,4 @@ public class Springbootquick2Application {
     public static void main(String[] args) {
         SpringApplication.run(Springbootquick2Application.class, args);
     }
-
 }
---
+++ springbootquick2/src/main/java/cn/school/controller/TestController.java
@@ -0,0 +1,15 @@
+package cn.school.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+    @RequestMapping("/test")
+    public ModelAndView gotoTest(){
+        ModelAndView mv=new ModelAndView();
+        String init="你好，欢迎来到德莱联盟";
+        return mv;
+    }
+}
